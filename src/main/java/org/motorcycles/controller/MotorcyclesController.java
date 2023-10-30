ea25bdd01049680f6d87d3031de0bc27bf70f7d5
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2023-10-30 05:12:02 -0300

    Remove motorcycle list from ram memory

--- src/main/java/org/motorcycles/controller/MotorcyclesController.java
+++ src/main/java/org/motorcycles/controller/MotorcyclesController.java
@@ -15,14 +15,9 @@ import jakarta.ws.rs.core.Response;
 import org.motorcycles.model.Motorcycle;
 import org.motorcycles.service.MotorcyclesService;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Path("/motorcycles")
 @RequestScoped
 public class MotorcyclesController {
-
-    private final Map<Long, Motorcycle> motorcyclesList = new HashMap<>();
 
     private final Gson gson = new Gson();
 
@@ -31,7 +26,6 @@ public class MotorcyclesController {
 
     public MotorcyclesController(MotorcyclesService motorcyclesService) {
         this.motorcyclesService = motorcyclesService;
-        this.populate();
     }
 
     @GET
@@ -71,30 +65,5 @@ public class MotorcyclesController {
 
     private Motorcycle getFromJson(String updatedMotorcycleJson) {
         return gson.fromJson(updatedMotorcycleJson, Motorcycle.class);
-    }
-
-
-    private Long generateUniqueId() {
-        return motorcyclesList.size() + 1L;
-    }
-
-
-    private void populate() {
-        motorcyclesList.put(1L, new Motorcycle(1L, "Harley-Davidson", "Sportster Iron 883",
-                2022, 53, "68 Nm", "Cruiser-style motorcycle with a classic look."));
-        motorcyclesList.put(2L, new Motorcycle(2L, "Honda", "CBR600RR",
-                2021, 120, "66 Nm", "Sportbike known for its performance and agility."));
-        motorcyclesList.put(3L, new Motorcycle(3L, "Ducati", "Panigale V4",
-                2023, 214, "124 Nm", "High-performance sportbike with cutting-edge technology."));
-        motorcyclesList.put(4L, new Motorcycle(4L, "Yamaha", "YZF-R3",
-                2022, 42, "29.6 Nm", "Lightweight sportbike suitable for beginners."));
-        motorcyclesList.put(5L, new Motorcycle(5L, "Kawasaki", "Ninja 400",
-                2021, 49, "38 Nm", "Versatile and learner-friendly sportbike."));
-        motorcyclesList.put(6L, new Motorcycle(6L, "BMW", "S1000RR",
-                2023, 205, "113 Nm", "Superbike with advanced electronics and performance."));
-    }
-
-    public Map<Long, Motorcycle> getMotorcyclesList() {
-        return motorcyclesList;
     }
 }
